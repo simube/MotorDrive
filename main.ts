d705569f76687ba8d39ab0e1407d97060515f2ca
simube <7550745+simube@users.noreply.github.com>
2022-10-09 12:56:15 +0200

    v1.0.3

--- main.ts
+++ main.ts
@@ -20,6 +20,17 @@ namespace motorbit {
         return motor;
     }
     
+
+    /**
+    * TODO: describe your function here
+    */
+    //% blockId=motordrive_activate block="activate MotorDrive"
+    export function activate(): void {
+
+        pins.digitalWritePin(DigitalPin.P14, 1)
+
+    }
+    
     /**
     * TODO: describe your function here
     * @param n the n from 0 (min) to 100 (max), eg:0
@@ -27,12 +38,20 @@ namespace motorbit {
     //% blockId=motordrive_forwards block="move %motors|forward with speed %n"
     //% n.min=0 n.max=100
     export function forwards(motors: motor, n: number): void {
-        // Add code here
 
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P13, 1)
-
-        pins.analogWritePin(AnalogPin.P1, n * 1023 / 100)
+        if (motor === 1) {
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            pins.digitalWritePin(DigitalPin.P13, 1)
+    
+            pins.analogWritePin(AnalogPin.P1, n * 1023 / 100)
+            
+        } else {
+            pins.digitalWritePin(DigitalPin.P15, 0)
+            pins.digitalWritePin(DigitalPin.P16, 1)
+    
+            pins.analogWritePin(AnalogPin.P2, n * 1023 / 100)
+            
+        }
 
     }
 
@@ -44,12 +63,20 @@ namespace motorbit {
     //% blockId=motordrive_backwards block="move %motors|backwards with speed %n"
     //% n.min=0 n.max=100
     export function backwards(motors: motor, n: number): void {
-        // Add code here
 
-        pins.digitalWritePin(DigitalPin.P12, 1)
-        pins.digitalWritePin(DigitalPin.P13, 0)
-
-        pins.analogWritePin(AnalogPin.P1, n * 1023 / 100)
+        if (motor === 1) {
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            pins.digitalWritePin(DigitalPin.P13, 0)
+    
+            pins.analogWritePin(AnalogPin.P1, n * 1023 / 100)
+            
+        } else {
+            pins.digitalWritePin(DigitalPin.P15, 1)
+            pins.digitalWritePin(DigitalPin.P16, 0)
+    
+            pins.analogWritePin(AnalogPin.P2, n * 1023 / 100)
+            
+        }
 
     }
 
